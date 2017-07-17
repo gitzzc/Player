3707ec6465ef9d77d2193f1b282477aa5c12b06c
gitzzc <ahzzc@sina.com>
2017-07-17 15:11:40 +0800

    S9 OK.

--- Player/Src/display_8807GCT.c
+++ Player/Src/display_8807GCT.c
@@ -48,11 +48,12 @@ void MenuUpdate(BIKE_STATUS* bike)
 	if( bike->CRZLight	) BL_Data[ 1] 	|= 0x10;	//S1
 	if( bike->ECO 		) BL_Data[ 4] 	|= 0x01;	//S3
 	if( bike->R 		) BL_Data[ 5] 	|= 0x01;	//S4
-	//if( bike->HallERR	) BL_Data[5] 	|= 0x08;	//S		电机霍尔故障
-	//if( bike->WheelERR) BL_Data[5] 	|= 0x04;	//S		手把故障
-	//if( bike->ECUERR 	) BL_Data[15]	|= 0x20;	//S 	电机控制器故障
+	//if( bike->HallERR	) BL_Data[5] 	|= 0x08;	//S?	电机霍尔故障
+	//if( bike->WheelERR) BL_Data[5] 	|= 0x04;	//S?	手把故障
+	//if( bike->ECUERR 	) BL_Data[15]	|= 0x20;	//S? 	电机控制器故障
+	//if( bike->PhaseERR) BL_Data[12]	|= 0x80;	//S? 	电机缺相故障
+	if ( bike->Braked	) BL_Data[12]	|= 0x80;	//S9 	
 
-	if( bike->PhaseERR  ) BL_Data[12]	|= 0x80;	//S9 	电机缺相故障
 	if( bike->Charge	) BL_Data[18]	|= 0x08;	//S10
 	if		( bike->PlayMedia == PM_FM 	) BL_Data[16] |= 0x04;	//S16
 	else if ( bike->PlayMedia == PM_USB ) BL_Data[16] |= 0x08;	//S14
